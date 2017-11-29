b46327bea45b587c8bb7c5b128790fbe625ec5c0
joseph <isaac@josephs-MacBook-Pro.local>
2017-11-29 12:00:28 -0700

    updates to entry

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -52,18 +52,20 @@ export default class App extends React.Component<any, any> {
         
     return (
         <Recoil overflow nightmode={appStore.nightmode}>
-            <Layer overflow fill flex className="p10">
-                <Open className="border-bottom" if={messagesExist} openToHeight={"42px"}>
-                    <Toolbar flex block textCenter flush>
-                        <Button materialIcon block disabled={!messagesExist} icon={"close"} onClick={this.clearAllMessages.bind(this)} className="mb10">
-                            Clear all messages
-                        </Button>
-                        <Button materialIcon  block disabled={!messagesExist} icon={"mood"} onClick={this.toggleNightmode.bind(this)} className="mb10">
-                            Toggle nightmode
-                        </Button>      
-                    </Toolbar>
-                </Open>
-                <Layer className="ptb20" block fill scrollY scrollToId={!messageStoreLoaded && messageStore.shouldScroll ? 'bottom' : ''} scrollIf={messageStore.shouldScroll}>
+            <Layer overflow fill flex>
+                <Layer>
+                    <Open className="border-bottom" if={messagesExist} openToHeight={"52px"}>
+                        <Toolbar flex block textCenter flush className="p10">
+                            <Button materialIcon block disabled={!messagesExist} icon={"close"} onClick={this.clearAllMessages.bind(this)}>
+                                Clear all messages
+                            </Button>
+                            <Button materialIcon  block disabled={!messagesExist} icon={"mood"} onClick={this.toggleNightmode.bind(this)}>
+                                Toggle nightmode
+                            </Button>      
+                        </Toolbar>
+                    </Open>
+                </Layer>
+                <Layer block fill scrollY scrollToId={!messageStoreLoaded && messageStore.shouldScroll ? 'bottom' : ''} scrollIf={messageStore.shouldScroll}>
                     {!messagesExist ? 
                     <Toolbar block textCenter>
                          <p><i className="fa super fa-commenting" /></p>
@@ -74,8 +76,8 @@ export default class App extends React.Component<any, any> {
                         return (
                             <Open key={item.id} if={removingItemId !== item.id} openToHeight="61px">
                                 <Emerge>
-                                    <Toolbar block className="p10 border-bottom">
-                                        <Button materialIcon className="p0" simple icon="close" size="large" onClick={this.removeMessage.bind(this, item.id)} />
+                                    <Toolbar onClick={this.removeMessage.bind(this, item.id)} block className="p10 border-bottom">
+                                        <Button materialIcon className="p0" simple icon="close" size="large" />
                                         <Button className="ps10" size="large" simple>{item.title}</Button>
                                     </Toolbar>
                                 </Emerge>
@@ -84,21 +86,20 @@ export default class App extends React.Component<any, any> {
                     })}
                     <div id="bottom"></div>
                 </Layer>
-                <Toolbar size="large" form block flex flush className="p5 border-top">
-                    <Button materialIcon className="w80px" icon={messageStore.typing ? "commenting": "face"} />
-                    <Input size="large" placeholder="Add a message.." block value={messageStore.message} onChange={this.setValue.bind(this)} />
-                    <Button materialIcon className="w80px" theme="primary" disabled={messageStore.message === ''} icon="add" onClick={this.addMessage.bind(this)} submit />
-                </Toolbar>
-            
-                <SlideIn className="z5" if={messageStore.loading} from="bottom" fill>
-                    <Layer fill flexCenter theme="light">
-                        <Loading size="xlarge" if={messageStore.loading} />
-                        <Button simple>loading...</Button>
-                    </Layer>
-            </SlideIn>
-
+               <Layer>
+                    <Toolbar size="large" form block flex flush className="p5 border-top">
+                        <Button materialIcon className="w80px" icon={messageStore.typing ? "commenting": "face"} />
+                        <Input size="large" placeholder="Add a message.." block value={messageStore.message} onChange={this.setValue.bind(this)} />
+                        <Button materialIcon className="w80px" theme="primary" disabled={messageStore.message === ''} icon="add" onClick={this.addMessage.bind(this)} submit />
+                    </Toolbar>
+                </Layer>
             </Layer>
-
+            <SlideIn className="z5" if={messageStore.loading} from="bottom" fill>
+                <Layer fill flexCenter theme="light">
+                    <Loading size="xlarge" if={messageStore.loading} />
+                    <Button simple>loading...</Button>
+                </Layer>
+            </SlideIn>
         </Recoil>
         )
     } 
